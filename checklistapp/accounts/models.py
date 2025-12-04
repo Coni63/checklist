8f48bee2366d7137d9da59347ff981220400e7f7
Nicolas M <mine.nicolas@gmail.com>
2025-12-04 22:02:14 +0100

    move model to accounts

--- checklistapp/accounts/models.py
+++ checklistapp/accounts/models.py
@@ -1,5 +1,29 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+
+
+class UserProjectPermissions(models.Model):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name="permissions")
+    project = models.ForeignKey("projects.Project", on_delete=models.CASCADE, related_name="permissions")
+    can_edit = models.BooleanField(default=False)
+    can_view = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ("user", "project")
+
+    def __str__(self):
+        if self.is_admin:
+            return f"{self.user.email} is admin of {self.project.name}"
+        elif self.can_edit:
+            return f"{self.user.email} can edit {self.project.name}"
+        elif self.can_view:
+            return f"{self.user.email} can view {self.project.name}"
+        else:
+            return f"{self.user.email} has no access to {self.project.name}"
